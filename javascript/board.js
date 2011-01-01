a44253043ffc29b794998d5ab98b445e205c9d7f
André Neubauer <andre.neubauer@googlemail.com>
2011-01-01 22:44:53 +0100

    initial commit

---
+++ javascript/board.js
@@ -0,0 +1,176 @@
+var story = function(name, prio) {
+	var name = name;
+	var prio = prio;
+	
+	return {
+		name: name,
+		prio: prio,
+		tasks: [],
+	
+		render: function() {
+			return $('<div></div>').text(name);
+		}
+	};
+}
+
+var task = function(name, state) {
+	var name;
+	var state;
+	
+	return {
+		name: name,
+		state: state,
+	
+		render: function() {
+			return $('<div></div>').addClass('task').text(name);
+		}
+	};
+}
+
+var board = function() {
+	// private
+	var stories = [];
+	var states = {
+		n: 'New',
+		pr: 'ProductManagement', 
+		d: 'Design', 
+		dev: 'Development', 
+		t: 'Test', 
+		r: 'Release'};
+	
+	function renderHeader() {
+		var header = $('<tr><th></th></tr>');
+		$.each(states, function(abbr, state) { 
+			header.append($('<th></th>').addClass('state').addClass(abbr).text(state));
+		});
+		return header;
+	}
+	
+	function renderStories() {
+		var storyBody = $('<tbody></tbody>');
+		$.each(stories, function(key, story) {
+			var row = $('<tr></tr>').attr('id', story.prio).
+				append($('<td></td>').addClass('story').
+				append(story.render()).
+				append($('<a></a>').attr('href','#task').text('+ Task').click(function() { 
+					$('#storyForm').hide();
+					$('#taskForm').show();
+				})));
+		
+			$.each(states, function(abbr, state) { 
+				var column = $('<td></td>');
+				
+				$.each(story.tasks, function(key, task) {
+					if(task.state == abbr) {
+						column.append(task.render());
+					}
+				});
+				
+				row.append(column);
+			});
+			
+			storyBody.append(row);
+		});
+		return storyBody;
+	}
+	
+	return {
+		// public interface
+		init: function(persistedStories) {
+			stories = persistedStories;
+		},
+		
+		addStory: function(story){
+			stories.push(story);
+		},
+		
+		addTask: function(task, story){
+			//stories.push(story);
+		},
+		
+		render: function(container){
+			$.each(states, function(abbr, state) {
+				 $('#states').append($('<option></option>').attr('value', abbr).text(state));	
+			});
+			$.each(stories, function(key, story) {
+				 $('#stories').append($('<option></option>').attr('value', story.name).text(story.name));
+			});
+
+			$('body').append($('<a></a>').attr('href','#story').text('+ Story').click(function() { 
+				$('#taskForm').hide();
+				$('#storyForm').show();
+			}));
+			
+			// board
+			$('body').append($('<table></table>').attr('id', 'board').append(renderHeader()).append(renderStories()));
+			$('#board').tableDnD({dragHandle: 'story', onDragClass: 'dragRow', 
+				onDrop: function(table, row) {
+					$('#board tr').not(':first').each(function(i, row) {
+						var prio = i+1;
+						var story = stories[row.id-1];
+						
+						if(prio != story.prio) {
+							dao.updatePriority(story.prio, prio);
+							story.prio = prio;
+						}
+					});
+				}
+			});
+			$('.story').hover(function() {
+				$(this).addClass('dragHandle');
+			}, function() {
+				$(this).removeClass('dragHandle');
+			});
+		}
+    };
+}();
+
+var dao = function() {
+	
+	if (!window.google || !google.gears) {
+		$('#feedback').append($('<span></span').addClass('error').text('Persistence not available'));
+	}
+		
+	return {
+		findAllStories: function() {
+			var x = new story('Do some stuff.', 1)
+			x.tasks.push(new task('Fix YSlow','pr'));
+			x.tasks.push(new task('Make application faster','dev'));
+			x.tasks.push(new task('Build something astonishing','pr'));
+			x.tasks.push(new task('Google Page Speed fix','d'));
+			
+			var y = new story('Rethink.', 2)
+			y.tasks.push(new task('Do something with OpenID','d'));
+			y.tasks.push(new task('Checkout optimize','dev'));
+			
+			var a = new story('Rethink.', 3)
+			a.tasks.push(new task('Credit Card Payment','r'));
+			a.tasks.push(new task('Rebuild with REST','dev'));
+			a.tasks.push(new task('Rebuild with SOAP','dev'));
+			
+			var b = new story('Rethink.', 4)
+			b.tasks.push(new task('Do something with OpenID','d'));
+			b.tasks.push(new task('Checkout optimize','dev'));
+			b.tasks.push(new task('Rebuild with REST','dev'));
+					
+			return [x, y, a, b];
+		},
+		
+		updatePriority: function(oPrio, nPrio) {
+			//alert('old: ' + oPrio + ', new: ' + nPrio);
+		}
+    };
+}();
+
+var addStory = function() {
+
+}
+
+var addTask = function() {
+
+}
+
+$(document).ready(function() {
+	board.init(dao.findAllStories());
+	board.render();
+});
