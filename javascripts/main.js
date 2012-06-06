9eb2990a62ad54f7ea3e10ee88b7352d413755b7
Daniel Manzke <daniel.manzke@googlemail.com>
2012-06-07 01:17:29 +0300

    Update gh-pages

--- javascripts/main.js
+++ javascripts/main.js
@@ -54,20 +54,7 @@ var board = function() {
 	function renderStory(story) {
 		var row = $('<li></li>').attr('id', story.prio).addClass('story');
 
-		// render story title
-		$('<div></div>').addClass('desc').addClass('.ui-widget-header').append(story.render()).appendTo(row);
-		
-		$.each(states, function(abbr, state) { 
-			var column = $('<ol></ol>').addClass('tasks').appendTo(row);
 			
-			// assign tasks
-			$.each(story.tasks, function(key, task) {
-				if(task.state == state.id) {
-					column.append(task.render());
-				}
-			});
-
-		});			
 		return row;
 	}
 		
@@ -77,50 +64,23 @@ var board = function() {
 		},
 
 		addState: function(state){
-			states.push(state);
+		
 		},
 
 		addStory: function(story){
-			//stories.push(story);
+			
 		},
 		
 		addTask: function(task, story){
-			story.addTask(task);
+			
 		},
 		
 		render: function(){
-			// create board
-			var board = $('<ol></ol>').attr('id', 'board');
-			
-			// render header
-			board.append($('<li></li>').addClass('desc').text(''));
-
-			$.each(states, function(s, state) {
-				board.append($('<li></li>').addClass('state').css("background-color", state.color).text(state.name)); //.addClass("ui-state-default")				
-			});		
-			
-			// render stories
-			$.each(stories, function(key, story) {
-				board.append(renderStory(story));
-			});
-			
-			// enable sortable
-			board.sortable();
-			board.disableSelection();
-			
-			// board
-			$('body').append(board);
-
-			// enable drag'n drop for tasks
-			$(".tasks").sortable({
-				connectWith: ".tasks"
-			}).disableSelection();
+		
 		},
 				
 		reset: function(container){
-			$('#board').remove();
-			stories.length=0;
-			states.length=0;
+			
 		}
     };
 }();
@@ -128,11 +88,7 @@ var board = function() {
 var dao = function(){
 	return {
 		save: function(location, isOnline){
-			if (window.localStorage) {
-				localStorage.setItem(location, JSON.stringify(board.getData()));
-			} else {
-				alert("Your Browser does not support LocalStorage.");
-			}	
+				
 		},
 		load: function(location, isOnline, store){
 			
