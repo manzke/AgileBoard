9aed1bfe26a0186ce78633ad226040e9f0aa0531
Daniel Manzke <daniel.manzke@googlemail.com>
2012-06-07 01:34:24 +0300

    Update gh-pages

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,30 +1,4 @@
-var isOnline = true;
 
-var state = function(id, name, color) {
-  var id;
-	var name;
-	var color;
-	
-	return {
-		id: id,
-		name: name,
-		color: color
-	};
-}
-
-var task = function(name, state) {
-	var name;
-	var state;
-	
-	return {
-		name: name,
-		state: state,
-	
-		render: function() {
-			return $('<li></li>').text(name);
-		}
-	};
-}
 
 	$( "#saveButton" )
 			.button()
